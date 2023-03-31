20d2b279aa3d306ae8517201333bb78b56893ab7
min <1820320652@qq.com>
2023-03-31 13:27:15 +0800

    完成写入method和uri

--- LogExtract.py
+++ LogExtract.py
@@ -5,32 +5,44 @@ import openpyxl
 
 
 pattern=re.compile(r'\[method\s*=\s*(\w+),\s*uri\s*=\s*([^,\s]*)')
-# pattern = re.compile(r'method = \'GET,\',uri = (.+)')
-
-start_time =datetime(2022, 1, 1, 0, 0, 1)
-end_time = datetime(2024, 1, 1, 0, 0, 3)
+timePattern = re.compile(r'\s*\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2}')
+start_time =datetime.strptime('2023-03-09 00:00:00','%Y-%m-%d %H:%M:%S')
+end_time = datetime.strptime('2023-03-11 00:00:00','%Y-%m-%d %H:%M:%S')
 
 results = []
+lines_without_time = []
 with open('D:/testLog.txt', 'r',encoding='UTF-8') as f:
-    for line in f:
-        log_time = datetime.strptime(line[0:19], '%Y-%m-%d %H:%M:%S')
-        if start_time <= log_time <= end_time:
-            match = pattern.search(line)
-            while match:
-                result ={
-                    'method' : match.group(1),
-                    'uri' : match.group(2)
-                }
-                results.append(result)
-                if results:
-                    print(results)  # 输出: [{'method': 'GET', 'uri': '/module-configs/personnel'}]
+    logs=f.readlines()
+    for log in logs:
+        match_time = timePattern.search(log)
+        if match_time:
+            log_time = match_time.group()
+            try:
+                logTime  = datetime.strptime(log_time, '%Y-%m-%d %H:%M:%S')
+            except ValueError:
+                continue
 
-                    # 将结果写入 Excel 文件
-                    wb = openpyxl.Workbook()
-                    ws = wb.active
-                    ws.append(['Method', 'URI'])
-                    for result in results:
-                        ws.append([result['method'], result['uri']])
-                    wb.save('results.xlsx')
-                else:
-                    print('没有匹配到结果')
+            if start_time <= logTime <= end_time:
+                match_request = pattern.search(log)
+                if match_request:
+                    result ={
+                        'method' : match_request.group(1),
+                        'uri' : match_request.group(2)
+                    }
+                    results.append(result)
+                    print(results)
+
+
+if results:
+    print(results)  # 输出: [{'method': ', 'uri': ']# 将结果写入 Excel 文件
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.append(['Method', 'URI'])
+    for result in results:
+        ws.append([result['method'], result['uri']])
+    try:
+        wb.save('results.xlsx')
+    except PermissionError:
+        print(f'文件保存失败：没有写入权限')
+else:
+    print('没有匹配到结果')
--- demo.py
+++ demo.py
@@ -1,16 +1,23 @@
 import re
-
+import datetime
 # pattern = re.compile(r'(?<=method = ).*?(?=,)')
 # pattern = re.compile(r'method = (\w+) uri (\w+)')
 # pattern=re.compile(r'(?<=method = )(\w+), uri = (\S+)')
-pattern=re.compile(r'\[method\s*=\s*(\w+),\s*uri\s*=\s*(\S+)')
-print('2023-03-10 00:00:00.002 - delete basic data cache [method = GET, uri = /module-configs/personnel/villager-schema,')
-s=pattern.search('2023-03-10 00:00:00.002 - delete basic data cache [method = GET, uri = /module-configs/personnel/villager-schema,')
+# pattern=re.compile(r'\[method\s*=\s*(\w+),\s*uri\s*=\s*(\S+)')
+# pattern = re.compile(r'^(\d{4}-\d{2}-\d{2})\s+(\d{2}:\d{2}:\d{2}\.\d{3})\s+\[(.*?)\]\s+(\S+)\s+(.*?)$')
+# # print('2023-03-10 00:00:00.002 - delete basic data cache [method = GET, uri = /module-configs/personnel/villager-schema,')
+# s=pattern.search('2023-03-10 00:00:00.002 [scheduling-1] INFO c.a.q.s.handler.ClientIndexStatisticHandler - delete basic data cache 2023-03-10 00:00:00.014 [scheduling-1] INFO org.ehcache.core.EhcacheManager - C[method = GET, uri = /module-configs/personnel/point-card, Azale method')
 
 # if s:
-print(s.group(1))
-print(s.group(2))
-# else:
-#     print('没有匹配到结果')
-# print('')
+# print(s.group(5))
+
+timePatten = re.compile(r'\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2}\d{3}')
+with open('D:/testLog.txt', 'r',encoding='UTF-8') as f:
+    logs = f.readlines()
+    for log in logs:
+        time_match=timePatten.search(log)
+        if time_match:
+            log_time = time_match.group()
+            logTime  = datetime.strptime(log_time, '%Y-%m-%d %H:%M:%S.%f')
+            print(logTime)
 
