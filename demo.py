adf6847841bac3e3cf979ec45e3c300af1005c19
min <1820320652@qq.com>
2023-04-06 15:34:17 +0800

    循环读取所有文件

--- demo.py
+++ demo.py
@@ -1,32 +1,64 @@
-import re
-import datetime
-# pattern = re.compile(r'(?<=method = ).*?(?=,)')
-# pattern = re.compile(r'method = (\w+) uri (\w+)')
-# pattern=re.compile(r'(?<=method = )(\w+), uri = (\S+)')
-# pattern=re.compile(r'\[method\s*=\s*(\w+),\s*uri\s*=\s*(\S+)')
-# pattern = re.compile(r'^(\d{4}-\d{2}-\d{2})\s+(\d{2}:\d{2}:\d{2}\.\d{3})\s+\[(.*?)\]\s+(\S+)\s+(.*?)$')
-# # print('2023-03-10 00:00:00.002 - delete basic data cache [method = GET, uri = /module-configs/personnel/villager-schema,')
-# s=pattern.search('2023-03-10 00:00:00.002 [scheduling-1] INFO c.a.q.s.handler.ClientIndexStatisticHandler - delete basic data cache 2023-03-10 00:00:00.014 [scheduling-1] INFO org.ehcache.core.EhcacheManager - C[method = GET, uri = /module-configs/personnel/point-card, Azale method')
+import re,os
+from datetime import datetime
+import openpyxl
+from collections import defaultdict, Counter
 
-# if s:
-# print(s.group(5))
-
-# timePatten = re.compile(r'\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2}\d{3}')
-# with open('D:/testLog.txt', 'r',encoding='UTF-8') as f:
-#     logs = f.readlines()
-#     for log in logs:
-#         time_match=timePatten.search(log)
-#         if time_match:
-#             log_time = time_match.group()
-#             logTime  = datetime.strptime(log_time, '%Y-%m-%d %H:%M:%S.%f')
-#             print(logTime)
-
-
-import os
 
 logPath='D:/testLog'
 fileList=os.listdir(logPath)
+# 定义正则表达式
+time_regex = re.compile(r'\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2}')
 
+# 读取日志文件
 for file in fileList:
-    with open(os.path.join(logPath,file)) as f:
+    with open(os.path.join(logPath,file), 'r',encoding='utf-8') as f:
+        logs = f.readlines()
         print(file)
+        f.close()
+
+# 需要统计的时间段
+start_time =datetime.strptime('2023-03-09 00:00:00','%Y-%m-%d %H:%M:%S')
+end_time = datetime.strptime('2023-03-11 00:00:00','%Y-%m-%d %H:%M:%S')
+
+# 定义字典，用于存储分类后的日志记录
+logs_dict = defaultdict(int)
+
+# 遍历日志文件中的每一行记录
+for log in logs:
+    # 使用正则表达式匹配时间
+    time_match = time_regex.search(log)
+    if time_match:
+        date_str = time_match.group()
+        # 将字符串类型的时间转换成 datetime.datetime 类型的对象
+        date_obj = datetime.strptime(date_str,  '%Y-%m-%d %H:%M:%S')
+        # 判断时间是否在指定的时间段内
+        if start_time <= date_obj <= end_time:
+            # 使用正则表达式匹配请求类型和 URL
+            match_result = re.search(r'\[method\s*=\s*(\w+),\s*uri\s*=\s*([^,\s]*)', log)
+            if match_result:
+                # 获取请求类型和 URL
+                method = match_result.group(1)
+                url = match_result.group(2)
+                # 将 URL 中的数字替换为 ID
+                url_new = re.sub(r'/(\d+)', '/id', url)
+                # 将请求类型和 URL 作为键，增加请求次数
+                logs_dict[(method, url_new)] += 1
+                # print(url_new)
+                print(logs_dict)
+
+# 将结果输出到 Excel 文件中
+wb = openpyxl.Workbook()
+ws = wb.active
+ws.title = 'Logs'
+
+ws.append(['method','url','count'])
+for (method,url),count in logs_dict.items():
+    ws.append([method, url, count])
+
+# 保存 Excel 文件
+try:
+    wb.save('results.xlsx')
+except PermissionError:
+        print(f'文件保存失败：没有写入权限')
+else:
+    print('没有匹配到结果')
